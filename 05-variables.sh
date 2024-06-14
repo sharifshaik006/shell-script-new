1b24e54c2b33e4ab84fd3937f60dfa880e239847
Nagul Shaik <NShaik@L2P42TW2XC.cust.communityfibre.co.uk>
2024-06-14 09:01:07 +0100

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -9,3 +9,12 @@ echo "enter password is "
 read PASSWORD 
 
 echo "my username is  my password is"
+
+
+echo "please enter username is "
+read -s USERNAME # here read is the command in the shell and USERNAME IS the variable read -s is where the data wont be visible
+
+echo "enter password is "
+read-s PASSWORD 
+
+echo "my username is ${USERNAME}  my password is ${PASSWORD}"
