d7c7c9ccb6bff03a53670f9f3bdf61198cd51efa
Nagul Shaik <NShaik@L2P42TW2XC.cust.communityfibre.co.uk>
2024-06-14 08:48:43 +0100

    variables

---
+++ 04-argumentsvariabl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# REFERRING VARIABLES WHICH MEANS  this is refering the variable PERSON
+PERSON1=$1
+PERSON2=$2
+
+
+
+# Here this is the declaring variable
+echo "${PERSON1}: hey how are you doing ${PERSON1}"
+echo "${PERSON2}: I am doing good and u ${PERSON2}?"
+echo "${PERSON1}: are you practising shell ${PERSON2}?"
+echo "${PERSON2}: yes  I am ${PERSON1}."
