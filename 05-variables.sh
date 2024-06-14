f1071541f7500a96bc1e36fca167325598a732c8
Nagul Shaik <NShaik@L2P42TW2XC.cust.communityfibre.co.uk>
2024-06-14 08:58:32 +0100

    variables

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Never hardcode the sensitive details
+
+echo "please enter username is "
+read USERNAME # here read is the command in the shell and USERNAME IS the variable 
+
+echo "enter password is "
+read PASSWORD 
+
+echo "my username is  my password is 
