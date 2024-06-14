cada4eb9900c8a670ee2ccc47518409b64cfca00
Nagul Shaik <NShaik@L2P42TW2XC.cust.communityfibre.co.uk>
2024-06-14 09:39:20 +0100

    variables

---
+++ 07-datatypes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#operations in the datatypes will be used in the shell like to perform some operations like sum multiply and other basic stuff
+
+NO1=$1
+NO2=$2
+
+SUM=$(($NO1+$NO2))
+
+echo "Total of $NO1 and $NO2 is: $SUM"
