bc5e8fcd419dccffb32178123b79b8cc67013f54
Nagul Shaik <NShaik@L2P42TW2XC.cust.communityfibre.co.uk>
2024-06-14 09:33:02 +0100

    variables

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-MOVIE= ("rrr" "pushpa" "murrari")
+MOVIE=("rrr" "pushpa" "murrari")
 
 #size of the above array is 3
 # index are 0,1,2
 
 # list always start with 0
 
-echo "First Movie is: ${MOVIES[0]}"
-echo "second movie is: ${MOVIES[1]}"
-echo  "All every is: ${MOVIES[@]}"
+echo "First Movie is: ${MOVIE[0]}"
+echo "second movie is: ${MOVIE[1]}"
+echo  "All every is: ${MOVIE[@]}"
