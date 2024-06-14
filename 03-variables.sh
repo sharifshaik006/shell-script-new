15dc563949546d5c657e13ef05be8a03c220b3fa
Nagul Shaik <NShaik@L2P42TW2XC.cust.communityfibre.co.uk>
2024-06-14 08:36:16 +0100

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=suresh 
+
+echo "Ramesh: hey how are you doing"
+echo "suresh: I am doing good and u?"
+echo "Ramesh: are you practising shell?"
+echo "suresh: yes  I am."
