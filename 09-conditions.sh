42c255d99536313d9e5e845204e24c4318f53159
Nagul Shaik <NShaik@L2P42TW2XC.cust.communityfibre.co.uk>
2024-06-14 10:05:08 +0100

    variables

---
+++ 09-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER =$1
+#-gt, -lt, -eq, -ge, -le
+
+if [$NUMBER -gt 10]
+then 
+     echo "the given $NUMBER is greater than 10"
+else
+    echo "Given number $NUMBER is less than 10"
+fi        
