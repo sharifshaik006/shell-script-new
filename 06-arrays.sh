6c2d86030f3f263e291b63cacbdc21929667ed6a
Nagul Shaik <NShaik@L2P42TW2XC.cust.communityfibre.co.uk>
2024-06-14 09:31:37 +0100

    variables

---
+++ 06-arrays.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MOVIE= ("rrr" "pushpa" "murrari")
+
+#size of the above array is 3
+# index are 0,1,2
+
+# list always start with 0
+
+echo "First Movie is: ${MOVIES[0]}"
+echo "second movie is: ${MOVIES[1]}"
+echo  "All every is: ${MOVIES[@]}"
